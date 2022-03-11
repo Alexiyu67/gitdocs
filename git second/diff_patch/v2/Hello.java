c8967f7abbea0b052db6129141f92cb4cc25f171
Alexander Trampe <alexander.trampe@gmx.de>
2022-03-11 15:27:05 +0100

    pls work

---
+++ git second/diff_patch/v2/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+
+    private final String msg = "Hello, world.";
+
+    public static void main(String[] args) {
+      Hello hello = new Hello();
+      System.out.println(hello.msg);
+    }
+}
